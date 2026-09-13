beb08d1061b81196c89437197596ed350e739823
Vincenzo Mecca <vincenzo.mecca@4science.com>
2026-09-13 11:02:42 +0200

    PROBE E7: new test file must be selected (F3)

---
+++ dspace-api/src/test/java/org/dspace/impact/NewProbeTest.java
@@ -0,0 +1,13 @@
+package org.dspace.impact;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class NewProbeTest {
+
+    @Test
+    void probe() {
+        assertTrue(true);
+    }
+}
